c0fdbae761664c208990a8282ed17f2fa9884709
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-04-24 14:50:01 +0200

    Added maven project structure

---
+++ ir-user-profile/src/main/java/com/kth-ir/project/App.java
@@ -0,0 +1,13 @@
+package com.kth-ir.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
