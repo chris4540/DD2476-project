9c7f35260c0381cbe2726e4520f0a9b61bb6f54e
Chris Lin <chris4540@gmail.com>
2019-05-02 12:48:56 +0200

    Use with statement to log the user profile

--- usr_profile_lib/usr_profile_log.py
+++ usr_profile_lib/usr_profile_log.py
@@ -1,18 +1,26 @@
 import logging
 import time
+import sqlite3
 
 logger = logging.getLogger('UserProfileLogger')
 
 class UserProfileLogger:
 
-    def __init__(self, user_email, dbconn=None):
-        if dbconn is None:
-            raise NotImplementedError("Not implemented to get connection from pool!")
-        else:
-            self.conn = dbconn
+    USER_PROFILE_DB = "usr_profile_db/user_profile.db"
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        logger.debug("Closing the database connection")
+        self.conn.close()
+
+    def __init__(self, user_email):
+        self.conn = sqlite3.connect(self.USER_PROFILE_DB)
+        logger.info("Using the profile database: %s", self.USER_PROFILE_DB)
 
         self.user_id = self._get_user_id(user_email)
-        logger.debug("Initialized UserProfileLogger: user_id = %d", self.user_id)
+        logger.debug("Initialized UserProfileLogger: user_id = %s", self.user_id)
 
     def _get_user_id(self, user_email):
         sql = "SELECT id FROM users WHERE email='{}';".format(user_email)
@@ -20,8 +28,12 @@ class UserProfileLogger:
         cur.execute(sql)
         row = cur.fetchone()
 
-        # parse user id
-        ret = row[0]
+        if row is None:
+            logger.info("The user email not found in the database. No profiling for the user")
+            ret = None
+        else:
+            # parse user id
+            ret = row[0]
         return ret
 
     @staticmethod
@@ -38,6 +50,9 @@ class UserProfileLogger:
         db_conn.commit()
 
     def log_search(self, query, query_type, ranking_type=None):
+        if self.user_id is None:
+            return
+
         # build col_val dict
         col_val = {
             'userid': str(self.user_id),
@@ -46,12 +61,37 @@ class UserProfileLogger:
             'query_type': query_type,
             'ranking_type': ranking_type
         }
+        # log the dictionary for debugging
+        logger.debug('[user_search_log] profile: %s', col_val)
         self._insert_col_val_to_db_table(self.conn, "user_search_log", col_val)
 
     def log_retrieved(self, selected_doc):
+        if self.user_id is None:
+            return
         col_val = {
             'userid': str(self.user_id),
             'posix_time': str(int(time.time())),
             'selected_doc': selected_doc
         }
+        logger.debug('[user_retrieved_log] profile: %s', col_val)
         self._insert_col_val_to_db_table(self.conn, "user_retrieved_log", col_val)
+
+if __name__ == "__main__":
+    import sqlite3
+    from usr_profile_lib.usr_profile_log import UserProfileLogger
+    logging.basicConfig(level=logging.DEBUG)
+    usr_email = "chlin3@kth.se"
+    # Developers can alway change the profile db like this
+    # UserProfileLogger.USER_PROFILE_DB = "myfoler/userprofile.db"
+    # ========================================================================
+    with UserProfileLogger(usr_email) as profile_logger:
+        # log search
+        profile_logger.log_search(
+            query="zombie", query_type="intersection", ranking_type=None)
+        profile_logger.log_search(
+            query="zombie attack", query_type="phase", ranking_type=None)
+        profile_logger.log_search(
+            query="zombie attack", query_type="ranking", ranking_type="tf-idf")
+
+        # log retrieved
+        profile_logger.log_retrieved(selected_doc="Zombie_Walk.f")
