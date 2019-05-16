35fda6c0a553a41a073ce72b236e264421ba762a
Chris Lin <chris4540@gmail.com>
2019-05-16 10:56:52 +0200

    Add exp2 results

---
+++ experiments/exp2/exp2.md
@@ -0,0 +1,18 @@
+Experiment 2. Personalization
+-------------------------------
+Secondly, We simulated the profiles being updated by
+clicking on relevant and documents and observed how their profile changed over time.
+
+
+% amerika <--- -ve impact for the target
+% matematik
+% anime  <---- +ve ?
+% programming
+% kina  <--- -ve impact for the target
+
+% Three queries to be tested
+% 1. castle (Slott)
+% 2. airport (flygplats)
+% 3. tempel
+
+
---
+++ experiments/exp2/setting2/config.py
@@ -0,0 +1,51 @@
+class Config:
+    # index = "enwiki"
+    index = "svwiki"
+    # weightings / relative importance between term vector
+    weights = {
+        "title": 2.0,
+        "category": 1.5,
+        "text": 1.0,
+    }
+    # decaying factor / half life (in second)
+    half_life = {
+        "query": 7*3600,
+        "title": 1*3600,
+        "category": 2*3600,
+        "text": 600,
+    }
+
+    elastic_host = "elastic.haochen.lu"
+    elastic_port = "9200"
+    timeout = 100
+    # elastic_host = "localhost"
+
+    doc_type = "page"  # TODO: add code comment for it
+
+    if index == "enwiki":
+        wiki_url_fmt = "http://en.wikipedia.org/wiki/{title}"
+    elif index == "svwiki":
+        wiki_url_fmt = "http://sv.wikipedia.org/wiki/{title}"
+
+    expansion_size = 100
+    feedback_weight = 1.0
+    boost = {
+        "title": 1.0,
+        "text": 1.0
+    }
+
+    profile_weights = {
+        "static": 0.3,
+        "dynamic": 0.7,
+    }
+
+    static_info_to_profile = {
+        # "lang": 1.0,
+        # "city": 1.0,
+        # "country": 1.0,
+    }
+
+    weight_scheme = "tfidf"
+    # the alpah value for re-ranking
+    rerank_alpha = 0.7
+    is_reorder_search_results = True  # reordering is a bit slow, option to turn it off
