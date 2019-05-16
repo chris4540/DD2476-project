ae65e417d813ce7d63bf55bde7918424c08d06e6
Chris Lin <chris4540@gmail.com>
2019-05-16 22:15:17 +0200

    Update the no-q-exp prec@20

--- experiments/exp3/plot.py
+++ experiments/exp3/plot.py
@@ -14,8 +14,16 @@ if __name__ == "__main__":
     ax = plot_results("nDCG20.csv")
     # relocate the lengend
     ax.legend(bbox_to_anchor=(1.1425, 1.03))
+    ax.set_ylim([0, 1])
     ax.set_ylabel("nDCG score")
-    # ax.set_title("Plot nDCG@20 against the number of clicks without query expansion")
     fig = ax.get_figure()
     fig.savefig("nDCG20_no_q_expand.png", bbox_inches='tight')
-    fig.savefig("nDCG20_no_q_expand.eps", format="eps", bbox_inches='tight')
+    fig.savefig("nDCG20_no_q_expand.svg", format="svg", bbox_inches='tight')
+
+    ax = plot_results("exp2-prec-20.csv")
+    ax.set_ylabel("Precision")
+    ax.set_ylim([0, 1.01])
+    # ax.set_title("Plot nDCG@20 against the number of clicks without query expansion")
+    fig = ax.get_figure()
+    fig.savefig("Prec20_no_q_expand.png", bbox_inches='tight')
+    fig.savefig("Prec20_no_q_expand.svg", format="svg", bbox_inches='tight')
--- experiments/plots/plot_all.py
+++ experiments/plots/plot_all.py
@@ -9,33 +9,35 @@ def plot_results(tsv):
     return ax
 if __name__ == "__main__":
 
-    # nDCG10
-    ax = plot_results("nDCG10.tsv")
-    ax.set_ylabel("nDCG score")
-    # ax.set_title("Plot nDCG@10 against the number of clicks")
-    fig = ax.get_figure()
-    fig.savefig("nDCG10.png", bbox_inches='tight')
-    fig.savefig("nDCG10.eps", format="eps", bbox_inches='tight')
-    # plt.cla()
+    # # nDCG10
+    # ax = plot_results("nDCG10.tsv")
+    # ax.set_ylabel("nDCG score")
+    # # ax.set_title("Plot nDCG@10 against the number of clicks")
+    # fig = ax.get_figure()
+    # fig.savefig("nDCG10.png", bbox_inches='tight')
+    # fig.savefig("nDCG10.eps", format="eps", bbox_inches='tight')
+    # # plt.cla()
     # nDCG20
     ax = plot_results("nDCG20.tsv")
     ax.set_ylabel("nDCG score")
+    ax.set_ylim([0, 1])
     # ax.set_title("Plot nDCG@20 against the number of clicks")
     fig = ax.get_figure()
     fig.savefig("nDCG20.png", bbox_inches='tight')
     fig.savefig("nDCG20.eps", format="eps", bbox_inches='tight')
 
     # Prec10
-    ax = plot_results("Prec10.tsv")
-    ax.set_ylabel("Precision")
-    # ax.set_title("Plot Precision at 10 against the number of clicks")
-    fig = ax.get_figure()
-    fig.savefig("Prec10.png", bbox_inches='tight')
-    fig.savefig("Prec10.eps", format="eps", bbox_inches='tight')
+    # ax = plot_results("Prec10.tsv")
+    # ax.set_ylabel("Precision")
+    # # ax.set_title("Plot Precision at 10 against the number of clicks")
+    # fig = ax.get_figure()
+    # fig.savefig("Prec10.png", bbox_inches='tight')
+    # fig.savefig("Prec10.eps", format="eps", bbox_inches='tight')
 
     # Prec20
     ax = plot_results("Prec20.tsv")
     ax.set_ylabel("Precision")
+    ax.set_ylim([0, 1])
     # ax.set_title("Plot Precision at 20 against the number of clicks")
     ax.legend(bbox_to_anchor=(1, .93))
     fig = ax.get_figure()
