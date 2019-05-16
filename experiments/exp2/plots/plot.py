e671bd32e33e46f3ca559ff0f7017c0c297b4ade
Chris Lin <chris4540@gmail.com>
2019-05-16 17:05:44 +0200

    Update the plot title

--- experiments/exp2/plots/plot.py
+++ experiments/exp2/plots/plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 if __name__ == '__main__':
     df = pd.read_csv("nDCG10.csv", index_col=0)
-    df.index.name = "Dynamic profile ratio"
+    df.index.name = "Dynamic profile ratio (β)"
     ax = df.plot(kind='bar', rot=0)
     ax.set_ylabel("nDCG score")
     ax.set_ylim([0, 1])
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     fig.savefig("nDCG10_exp3.svg", format="svg", bbox_inches='tight')
 
     df = pd.read_csv("nDCG20.csv", index_col=0)
-    df.index.name = "Dynamic profile ratio"
+    df.index.name = "Dynamic profile ratio (β)"
     ax = df.plot(kind='bar', rot=0)
     ax.set_ylabel("nDCG score")
     ax.set_ylim([0, 1])
