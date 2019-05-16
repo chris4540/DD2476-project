1ab01b9fccd45835975cd135f7707f31b12bc0c0
Chris Lin <chris4540@gmail.com>
2019-05-16 12:25:14 +0200

    Add annotation

---
+++ experiments/exp2/apply_annotate.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    # apply for tempel
+    annotate = pd.read_csv("tempel_rel.csv", header=None)
+    annotate.columns = ["rel", "name"]
+    annotate = annotate.set_index("name")
+    rel_dict = annotate["rel"].to_dict()
+    for i in range(1,5):
+        f = "setting{}/tempel_query.csv".format(i)
+        df = pd.read_csv(f, header=None, index_col=0)
+        df.columns = ['doc_id', 'name']
+        df['rel'] = df['name'].map(rel_dict, na_action='ignore')
+        df.to_csv(f, header=False)
+
+    # apply for the flygplats
+    annotate = pd.read_csv("flygplats_rel.csv", header=None)
+    annotate.columns = ["rel", "name"]
+    annotate = annotate.set_index("name")
+    rel_dict = annotate["rel"].to_dict()
+    for i in range(1,5):
+        f = "setting{}/flygplats_query.csv".format(i)
+        df = pd.read_csv(f, header=None, index_col=0)
+        df.columns = ['doc_id', 'name']
+        df['rel'] = df['name'].map(rel_dict, na_action='ignore')
+        df.to_csv(f, header=False)
+
+    # apply for the slott
+    annotate = pd.read_csv("slott_rel.csv", header=None)
+    annotate.columns = ["rel", "name"]
+    annotate = annotate.set_index("name")
+    rel_dict = annotate["rel"].to_dict()
+    for i in range(1,5):
+        f = "setting{}/slott_query.csv".format(i)
+        df = pd.read_csv(f, header=None, index_col=0)
+        df.columns = ['doc_id', 'name']
+        df['rel'] = df['name'].map(rel_dict, na_action='ignore')
+        df.to_csv(f, header=False)
