8bec1f4d1f4a5a4e68d0948244dcb60e08c1d09d
Chris Lin <chris4540@gmail.com>
2019-05-16 10:06:41 +0200

    Add plots

---
+++ experiments/plots/plot_all.py
@@ -0,0 +1,38 @@
+# import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_results(tsv):
+    df = pd.read_csv(tsv, sep="\t", index_col=0)
+    df.index.name = "clicks"
+    ax = df.plot(kind='line', style='x-')
+    return ax
+if __name__ == "__main__":
+
+    # nDCG10
+    ax = plot_results("nDCG10.tsv")
+    ax.set_ylabel("nDCG score")
+    ax.set_title("Plot nDCG@10 against the number of clicks")
+    fig = ax.get_figure()
+    fig.savefig("nDCG10.png", bbox_inches='tight')
+    # plt.cla()
+    # nDCG20
+    ax = plot_results("nDCG20.tsv")
+    ax.set_ylabel("nDCG score")
+    ax.set_title("Plot nDCG@20 against the number of clicks")
+    fig = ax.get_figure()
+    fig.savefig("nDCG20.png", bbox_inches='tight')
+
+    # Prec10
+    ax = plot_results("Prec10.tsv")
+    ax.set_ylabel("Precision")
+    ax.set_title("Plot Precision at 10 against the number of clicks")
+    fig = ax.get_figure()
+    fig.savefig("Prec10.png", bbox_inches='tight')
+
+    # Prec20
+    ax = plot_results("Prec20.tsv")
+    ax.set_ylabel("Precision")
+    ax.set_title("Plot Precision at 20 against the number of clicks")
+    fig = ax.get_figure()
+    fig.savefig("Prec20.png", bbox_inches='tight')
